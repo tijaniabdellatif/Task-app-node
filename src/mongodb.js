c849d81c353d2b2ea4dd99da4e0037f0d5a9c18a
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-16 02:06:31 +0100

    querying data

--- src/mongodb.js
+++ src/mongodb.js
@@ -4,89 +4,54 @@ const {MongoClient,ObjectId} = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectId();
-console.log(id.id.length)
-console.log(id.toHexString().length);
-console.log(id.getTimestamp());
-
 MongoClient.connect(connectionURL,{ useNewUrlParser : true },(error,client)  => {
 
+    
 
      if(error){
 
         return console.log('Unable to connect to database');
      }
 
-   
-
      const db = client.db(databaseName);
 
-    //  db.collection('users').insertOne({
+     db.collection("users").count({
 
-    //      _id:id,
-    //      name:'abdellatif tijani',
-    //      age:33
+          name:"TIJANI"
+     },(error,num) => {
 
-    //  },(error,result) => {
+        console.log("the count is : " + typeof(num));
+     });
 
-    //       if (error){
+     db.collection('users').countDocuments({
 
-    //         return console.log('Unable to insert user');
-    //       }
+          name:'amine'
+     },(error,result) => {
 
+         console.log("the result is : "+ result + " the type is : " +typeof(result));
+     })
 
-    //        console.log(result);
-    //  });
-     
-
-        // db.collection('users').insertMany([
-
-        //     {
-        //         name:'karim',
-        //         age:14,
-        //     },
-        //     {
-        //         name:"amine",
-        //         age:19
-        //     }
-        // ],(error,result) => {
-
-        //     if(error){
-
-        //         return console.log('unable to insert those');
-        //     }
-
-        //     console.log(result);
-        // })
-
-        // db.collection('tasks').insertMany([
-
-        //      {
-        //          title: "task #1",
-        //          description:"Making a camp",
-        //          date:new Date("2021-03-15"),
-        //          completed:true
-        //      },
-
-        //      {
-        //         title: "task #2",
-        //         description:"Eating a breafast",
-        //         date:new Date("2021-03-16"),
-        //         completed:false
-        //     }
-
-        // ],(error,result) => {
-
-        //     if(result.insertedCount === 2){
-
-        //         return console.log('inserted successfully');
-        //     }
-
-        //     else {
-
-        //         return console.log(error);
-        //     }
-        // });
+    /* Findone always return one record  */
+     db.collection('users').findOne({ _id: new ObjectId("62310e2ba376b89d5ca00351") },(error,user) => {
 
         
+        if(error){
+            return console.log('unable to fetch');   
+        }
+
+        console.log(user);
+
+     })
+
+     
+    
+   
+     /* Return a cursor a pointer to the data */
+     db.collection('users').find({ name:"TIJANI" }).toArray((error,users)=>{
+
+        console.log(users);
+     });
+    
+    
+    
 });
