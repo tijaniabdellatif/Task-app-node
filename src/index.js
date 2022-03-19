751f1d99cc0a75b4538b2fa2ca80ba274084bd1f
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-19 13:15:51 +0100

    fetch users and single user

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-require('./db/mongoose');
+const moongoose = require('./db/mongoose');
 const User = require('./models/User');
 const Task = require('./models/Task');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/users',(req,res) => {
-  
       const user = new User(req.body);
       user.save().then(() => {
         res.status(201).send(user);
@@ -18,8 +18,39 @@ app.post('/users',(req,res) => {
 });
 
 
-app.post('/tasks',(req,res) => {
+app.get('/users/:id',(req,res) => {
+      const _id = req.params.id
+      User.findById(_id).then((user) => {  
+             if(!user){
+                  return res.status(404).send({
+                    message:"No data provided",
+                    status:res.statusCode,
+                  });
+             }
 
+            res.status(200).send(user);
+
+      }).catch((error) => {
+
+           res.status(500).send(error);
+      })
+        
+})
+
+app.get('/users',(req,res) => {
+
+     User.find({}).then((users) => {
+
+         res.send(users);      
+
+     }).catch(error => {
+
+      res.status(500).send(error);
+
+     })
+})
+
+app.post('/tasks',(req,res) => {
     const task = new Task(req.body);
     task.save().then(() => {
          res.status(201).send(task)
