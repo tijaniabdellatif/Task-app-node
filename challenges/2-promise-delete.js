627d7b8a4cc3ca340a8769681f8e5c81c04291b7
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-20 01:31:29 +0100

    just some helpful tips

--- challenges/2-promise-delete.js
+++ challenges/2-promise-delete.js
@@ -1,21 +1,39 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/Task');
-const _id = '6235c0c7b8ce4f4f552ec118';
+const _id = '6235d00f346ba86ccea146d3';
 
-Task.findByIdAndDelete(_id).then((task) => {
-     if(task===null){
-         console.log("the task is deleted");
-     }
-      return Task.countDocuments({completed:false});
-}).then((result) => {
-       console.log("total number of imcomplete tasks : " + result)
-       return Task.findById(_id);
-}).then((result) => {
-    if(result === null){
-        return console.log('No data given ..');
-    }
-    console.log(result);
+// Task.findByIdAndDelete(_id).then((task) => {
+//      if(task===null){
+//          console.log("the task is deleted");
+//      }
+//       return Task.countDocuments({completed:false});
+// }).then((result) => {
+//        console.log("total number of imcomplete tasks : " + result)
+//        return Task.findById(_id);
+// }).then((result) => {
+//     if(result === null){
+//         return console.log('No data given ..');
+//     }
+//     console.log(result);
   
-}).catch(e => {
-    console.log(e);
+// }).catch(e => {
+//     console.log(e);
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+
+   const task = await Task.findByIdAndDelete(id);
+
+   if(task === null){
+    return console.log('No data provided');
+   }
+   const count = await Task.countDocuments({completed:false});
+   return count;
+}
+
+deleteTaskAndCount(_id).then(res => {
+    console.log(res);
+}).catch(error => {
+    console.log(error);
 })
