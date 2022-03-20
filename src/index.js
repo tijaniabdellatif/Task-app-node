530ce8e28ce8c03804c8e06c28f0615de73344df
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-20 01:57:57 +0100

    mutating user to async await

--- src/index.js
+++ src/index.js
@@ -8,39 +8,57 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users',(req,res) => {
-      const user = new User(req.body);
-      user.save().then(() => {
-        res.status(201).send(user);
-      }).catch((error) => {
-        res.status(400).send(error);
-      })
+app.post('/users',async (req,res) => {
+      const user =  new User(req.body);
+      try{
+        await user.save()
+        res.status(201).send(user)
+      }catch(error){
+           res.status(400).send({
+               status:res.statusCode,
+               error
+           });
+      }  
 });
 
-app.get('/users/:id',(req,res) => {
-      const _id = req.params.id
-      User.findById(_id).then((user) => {  
-             if(!user){
-                  return res.status(404).send({
-                    message:"No data provided",
-                    status:res.statusCode,
-                  });
-             }
-            res.status(200).send(user);
+app.get('/users/:id', async (req,res) => {
 
-      }).catch((error) => {
+  const _id = req.params.id
+  
+  try {
 
-           res.status(500).send(error);
-      })
-        
+          const user = await User.findById(_id);
+          if(!user){
+            return res.status(404).send({
+              status:res.statusCode,
+              headers : res.getHeaders()
+            })
+          }
+          res.status(200).send(user);
+  }
+  catch(error){
+    res.status(500).send({
+       status:res.statusCode,
+       message:error
+    })
+  }
+  
 })
 
-app.get('/users',(req,res) => {
-     User.find({}).then((users) => {
-         res.send(users);      
-     }).catch(error => {
-      res.status(500).send(error);
-     })
+app.get('/users',async (req,res) => {
+
+  try{
+
+    const users = await User.find({});
+    res.status(200).send(users);
+     
+  }catch(error){
+
+      res.status(500).send({
+        status:res.statusCode,
+        error
+      })
+  }
 })
 
 app.post('/tasks',(req,res) => {
