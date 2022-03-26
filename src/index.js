3f08a35fd466f1a43a5c0cc0477057e05c17619d
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-26 01:34:34 +0100

    hashing password

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ require('./db/mongoose');
 const Task = require('./models/Task');
 const userRouter = require('./routes/user');
 const taskRouter = require('./routes/task');
+
+
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
@@ -11,3 +14,21 @@ app.use(taskRouter);
 app.listen(port,() => {
   console.log('Server is up on port ' + port);
 });
+
+
+const bycrpt = require('bcryptjs');
+const myFunction =  async () => {
+
+  const password = 'tijani@@!'
+  const hashedPassword = await bycrpt.hash(password,8);
+   
+  console.log(password, hashedPassword)
+
+
+  const isMatch = await bycrpt.compare(password,hashedPassword);
+  console.log(isMatch);
+
+
+}
+
+myFunction();
